9b2f7e84110a5b693514e39dc56dbad1a5f29f63
Nils Ost <home@nijos.de>
2021-02-23 10:05:51 +0100

    Migration von Arduino zu PlatformIO; mplab prokejt in CoIC Lib verlagert; CoIC wird nun aus Lib verwendet

---
+++ platformio/NAHS-LatchBrick_v1.0/include/global.h
@@ -0,0 +1,37 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include <Arduino.h>
+#define NO_GLOBAL_COIC
+#include <CoIC.h>
+
+class configData;
+class runtimeData;
+
+#define COIC_ADDR 45
+
+#define SETUP_PIN D5
+#define BAT_CHRG_PIN D6
+#define BAT_STDBY_PIN D7
+#define BAT_ADC_PIN A0
+#define SDA_PIN D2
+#define SCL_PIN D1
+
+#define DEFAULT_DEEP_SLEEP_DELAY 60
+#define NUMBER_OF_FEATURES 3
+
+#define BRICK_TYPE 2
+#define NUMBER_OF_VERSIONS 5
+
+#define OS_YEAR 2021
+
+extern const String version_names[NUMBER_OF_VERSIONS];
+extern const float version_numbers[NUMBER_OF_VERSIONS];
+extern const String features[NUMBER_OF_FEATURES];
+extern configData *cfgdat;
+extern runtimeData *rundat;
+extern CoIC_Latch *latches;
+
+float readBatVoltage();
+
+#endif // GLOBAL_H
---
+++ platformio/NAHS-LatchBrick_v1.0/src/global.cpp
@@ -0,0 +1,20 @@
+#include "global.h"
+#include "configData.h"
+#include "runtimeData.h"
+
+const String version_names[NUMBER_OF_VERSIONS] = {"os", "all", "latch", "bat", "sleep"};
+const float version_numbers[NUMBER_OF_VERSIONS] = {1.0, 1.0, 1.0, 1.0, 1.0};
+const String features[NUMBER_OF_FEATURES] = {"latch", "bat", "sleep"};
+configData *cfgdat;
+runtimeData *rundat;
+CoIC_Latch *latches = new CoIC_Latch();
+
+float readBatVoltage() {
+    uint32_t adcReading = 0;
+    for(uint8_t i = 0; i < 3; i++) {
+      adcReading += analogRead(BAT_ADC_PIN);
+      delay(20);
+    }
+  adcReading /= 3;
+  return (((uint16_t) adcReading) * 5.0) / rundat->vars.adc5V;
+}
